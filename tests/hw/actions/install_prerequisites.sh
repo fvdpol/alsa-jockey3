a757378127edb3e8959d418e738d0dc17d3a52ed
Frank van de Pol <fvdpol@gmail.com>
2026-08-25 11:43:36 -0400

    update dependencies for test client

--- tests/hw/actions/install_prerequisites.sh
+++ tests/hw/actions/install_prerequisites.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+#
 # install the various prerequisite packages used by our testsuite
 #
 #
-sudo apt install uhubctl alsa-utils sox python3-mido python3-rtmidi
+sudo apt install uhubctl alsa-utils sox 
+sudo apt install linux-cpupower
+sudo apt install python3-mido python3-rtmidi python3-yaml
