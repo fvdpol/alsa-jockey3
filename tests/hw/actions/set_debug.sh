5ea951ab12c7d892ce2bd3701a15ef8604e18a8f
Frank van de Pol <fvdpol@gmail.com>
2026-09-01 09:11:46 -0400

    tests: disable console output for dmesg the printing of debug output to the console significantly impacts the timing of the driver if a serial console is used

--- tests/hw/actions/set_debug.sh
+++ tests/hw/actions/set_debug.sh
@@ -1,5 +1,9 @@
 echo "module snd_reloop_jockey3 +p" | sudo tee /sys/kernel/debug/dynamic_debug/control
 
+# disable printing kernel messages to console 
+# especially if serial console is used this causes a massive slowdown and influences timing
+sudo dmesg -n 1
+
 # 
 echo sudo sysctl -w kernel.printk="8 4 1 7"
 
